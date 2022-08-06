17997d97cb78d831d4fb5be9c83f1924a09c081b
Manh Pham <manhpham90vn@icloud.com>
2022-08-07 00:55:07 +0700

    Initial Commit

---
+++ LocalDataViewer/LocalDataViewer.h
@@ -0,0 +1,18 @@
+//
+//  LocalDataViewer.h
+//  LocalDataViewer
+//
+//  Created by Manh Pham on 8/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalDataViewer.
+FOUNDATION_EXPORT double LocalDataViewerVersionNumber;
+
+//! Project version string for LocalDataViewer.
+FOUNDATION_EXPORT const unsigned char LocalDataViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalDataViewer/PublicHeader.h>
+
+
